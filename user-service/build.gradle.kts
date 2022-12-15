b831e985bf354a8643605274bee70048371c7f60
hulkdx <hulktroll@gmail.com>
2022-12-15 21:40:48 +0200

    Using paketobuildpacks:tiny

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,57 +1,62 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
-plugins {
-	id("org.springframework.boot") version "3.0.0"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
-}
-
-val user = "hulkdx"
-group = "com.$user"
+group = "com.hulkdx"
 version = "1"
 java.sourceCompatibility = JavaVersion.VERSION_19
 
+plugins {
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.spring") version "1.7.21"
+}
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.liquibase:liquibase-core")
-	runtimeOnly("org.postgresql:postgresql")
-	runtimeOnly("org.postgresql:r2dbc-postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.liquibase:liquibase-core")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.postgresql:r2dbc-postgresql")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.0")
-	testImplementation("org.testcontainers:junit-jupiter:1.17.6")
-	testImplementation("org.testcontainers:postgresql:1.17.6")
-	testImplementation("org.testcontainers:r2dbc:1.17.6")
-	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.6")
+    testImplementation("org.testcontainers:postgresql:1.17.6")
+    testImplementation("org.testcontainers:r2dbc:1.17.6")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
 }
 
-tasks.withType<BootBuildImage> {
-	imageName.set(System.getenv("DOCKER_IMAGE_NAME"))
-}
+tasks {
+    bootBuildImage {
+        builder.set("paketobuildpacks/builder:tiny")
+        environment.set(
+            mapOf(
+                "BP_NATIVE_IMAGE" to "true",
+            )
+        )
+    }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "18"
-	}
-}
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "18"
+        }
+    }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+    jar {
+        enabled = false
+    }
 
-tasks.getByName<Jar>("jar") {
-	enabled = false
+    test {
+        useJUnitPlatform()
+    }
 }
