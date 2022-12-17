59bbfe3e0af6e7112a31b1e7d8232a3d7cdf2ef5
hulkdx <hulktroll@gmail.com>
2022-12-17 12:30:13 +0200

    Simple user register added, TODO: exception cases

--- user-service/src/main/kotlin/com/hulkdx/findprofessional/AuthController.kt
+++ user-service/src/main/kotlin/com/hulkdx/findprofessional/AuthController.kt
@@ -3,22 +3,32 @@ package com.hulkdx.findprofessional
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Controller
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 
-@Controller("auth")
-class AuthController {
-    @PostMapping("login")
-    suspend fun login() {
-    }
+@RestController
+@RequestMapping("/auth")
+class AuthController(
+    private val userRepository: UserRepository,
+) {
 
-    @PostMapping("register")
-    suspend fun register() {
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    suspend fun register(@RequestBody body: User) {
+        userRepository.save(body)
     }
 }
 
 interface UserRepository : CoroutineCrudRepository<User, Int>
 
 @Table("users")
-data class User(@Id val id: Int, val name: String)
+data class User(
+    val email: String,
+    val password: String,
+    @Id val id: Int? = null,
+)
