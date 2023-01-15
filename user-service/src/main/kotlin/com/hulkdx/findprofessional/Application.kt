0350501b6bfe7a35a53864842a7973e49e16bd89
hulkdx <hulktroll@gmail.com>
2023-01-15 23:23:34 +0200

    Refactoring: Create RuntimeHints.kt

--- user-service/src/main/kotlin/com/hulkdx/findprofessional/Application.kt
+++ user-service/src/main/kotlin/com/hulkdx/findprofessional/Application.kt
@@ -1,31 +1,14 @@
 package com.hulkdx.findprofessional
 
-import org.springframework.aot.hint.MemberCategory
-import org.springframework.aot.hint.RuntimeHints
-import org.springframework.aot.hint.RuntimeHintsRegistrar
+import com.hulkdx.findprofessional.config.RuntimeHints
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ImportRuntimeHints
-import org.springframework.jdbc.datasource.SimpleDriverDataSource
 
-@ImportRuntimeHints(Hints::class)
+@ImportRuntimeHints(RuntimeHints::class)
 @SpringBootApplication
 class FindProfessionalApplication
 
 fun main(args: Array<String>) {
     runApplication<FindProfessionalApplication>(*args)
-}
-
-class Hints : RuntimeHintsRegistrar {
-    override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
-        listOf(
-            ArrayList::class.java,
-            SimpleDriverDataSource::class.java,
-        ).forEach {
-            hints.reflection().registerType(it, *MemberCategory.values())
-        }
-        hints.resources().registerPattern("db/changelog/db.changelog-master.sql")
-        hints.resources().registerPattern("rsa.private.key")
-        hints.resources().registerPattern("rsa.public.key")
-    }
 }
---
+++ user-service/src/main/kotlin/com/hulkdx/findprofessional/config/RuntimeHints.kt
@@ -0,0 +1,20 @@
+package com.hulkdx.findprofessional.config
+
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.RuntimeHints
+import org.springframework.aot.hint.RuntimeHintsRegistrar
+import org.springframework.jdbc.datasource.SimpleDriverDataSource
+
+class RuntimeHints : RuntimeHintsRegistrar {
+    override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
+        listOf(
+            ArrayList::class.java,
+            SimpleDriverDataSource::class.java,
+        ).forEach {
+            hints.reflection().registerType(it, *MemberCategory.values())
+        }
+        hints.resources().registerPattern("db/changelog/db.changelog-master.sql")
+        hints.resources().registerPattern("rsa.private.key")
+        hints.resources().registerPattern("rsa.public.key")
+    }
+}
