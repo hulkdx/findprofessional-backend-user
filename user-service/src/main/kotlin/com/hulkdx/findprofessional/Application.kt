f02e54dd533095514d12c0f40c2a73ebaa1ad71f
hulkdx <hulktroll@gmail.com>
2023-01-16 15:33:33 +0200

    Refactoring

--- user-service/src/main/kotlin/com/hulkdx/findprofessional/Application.kt
+++ user-service/src/main/kotlin/com/hulkdx/findprofessional/Application.kt
@@ -1,15 +1,12 @@
 package com.hulkdx.findprofessional
 
 import com.hulkdx.findprofessional.config.RuntimeHints
-import org.springframework.boot.actuate.autoconfigure.web.reactive.ReactiveManagementContextAutoConfiguration
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ImportRuntimeHints
 
 @ImportRuntimeHints(RuntimeHints::class)
 @SpringBootApplication
-@ImportAutoConfiguration(ReactiveManagementContextAutoConfiguration::class)
 class FindProfessionalApplication
 
 fun main(args: Array<String>) {
