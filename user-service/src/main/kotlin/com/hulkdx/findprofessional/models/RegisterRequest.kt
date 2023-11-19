b25526249b4cb9417acad917a8c9877b3dbe9971
hulkdx <hulktroll@gmail.com>
2023-11-19 19:02:04 +0200

    Small change

--- user-service/src/main/kotlin/com/hulkdx/findprofessional/models/RegisterRequest.kt
+++ user-service/src/main/kotlin/com/hulkdx/findprofessional/models/RegisterRequest.kt
@@ -3,16 +3,11 @@ package com.hulkdx.findprofessional.models
 import jakarta.validation.constraints.Size
 
 data class RegisterRequest(
-    @field:Size(max = 50)
-    val email: String,
-    @field:Size(max = 50)
-    val password: String,
-    @field:Size(max = 50)
-    val firstName: String,
-    @field:Size(max = 50)
-    val lastName: String,
-    @field:Size(max = 50)
-    val profileImage: String?,
+    @field:Size(max = 50) val email: String,
+    @field:Size(max = 50) val password: String,
+    @field:Size(max = 50) val firstName: String,
+    @field:Size(max = 50) val lastName: String,
+    @field:Size(max = 50) val profileImage: String?,
 )
 
 fun RegisterRequest.toUser(password: String): User {
