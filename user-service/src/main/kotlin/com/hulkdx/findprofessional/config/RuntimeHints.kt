f2edab59bead8fcdbed54a5b9def3a245cfac04d
hulkdx <hulktroll@gmail.com>
2024-08-09 20:58:01 +0300

    Spring boot: 3.3.2 (#80)

--- user-service/src/main/kotlin/com/hulkdx/findprofessional/config/RuntimeHints.kt
+++ user-service/src/main/kotlin/com/hulkdx/findprofessional/config/RuntimeHints.kt
@@ -1,6 +1,12 @@
 package com.hulkdx.findprofessional.config
 
 import liquibase.changelog.ChangeLogHistoryServiceFactory
+import liquibase.changelog.FastCheckService
+import liquibase.changelog.visitor.ValidatingVisitorGeneratorFactory
+import liquibase.database.LiquibaseTableNamesFactory
+import liquibase.parser.SqlParserFactory
+import liquibase.report.ShowSummaryGeneratorFactory
+import liquibase.ui.LoggerUIService
 import org.springframework.aot.hint.MemberCategory
 import org.springframework.aot.hint.RuntimeHints
 import org.springframework.aot.hint.RuntimeHintsRegistrar
@@ -11,10 +17,23 @@ class RuntimeHints : RuntimeHintsRegistrar {
         listOf(
             ArrayList::class.java,
             SimpleDriverDataSource::class.java,
-            ChangeLogHistoryServiceFactory::class.java,
         ).forEach {
             hints.reflection().registerType(it, *MemberCategory.values())
         }
         hints.resources().registerPattern("db/changelog/db.changelog-master.sql")
+
+        // Might be fixed in
+        // https://github.com/oracle/graalvm-reachability-metadata/issues/431
+        listOf(
+            LoggerUIService::class.java,
+            ChangeLogHistoryServiceFactory::class.java,
+            FastCheckService::class.java,
+            LiquibaseTableNamesFactory::class.java,
+            ValidatingVisitorGeneratorFactory::class.java,
+            ShowSummaryGeneratorFactory::class.java,
+            SqlParserFactory::class.java,
+        ).forEach {
+            hints.reflection().registerType(it, *MemberCategory.values())
+        }
     }
 }
