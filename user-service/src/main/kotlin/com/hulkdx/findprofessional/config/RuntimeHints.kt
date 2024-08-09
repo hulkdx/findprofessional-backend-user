182dd326f6bff5e15f59c7f5ae5e3d8ece573ab9
hulkdx <hulktroll@gmail.com>
2024-08-09 15:04:06 +0300

    Fix liquibase

--- user-service/src/main/kotlin/com/hulkdx/findprofessional/config/RuntimeHints.kt
+++ user-service/src/main/kotlin/com/hulkdx/findprofessional/config/RuntimeHints.kt
@@ -1,6 +1,7 @@
 package com.hulkdx.findprofessional.config
 
 import liquibase.changelog.ChangeLogHistoryServiceFactory
+import liquibase.ui.LoggerUIService
 import org.springframework.aot.hint.MemberCategory
 import org.springframework.aot.hint.RuntimeHints
 import org.springframework.aot.hint.RuntimeHintsRegistrar
@@ -11,10 +12,18 @@ class RuntimeHints : RuntimeHintsRegistrar {
         listOf(
             ArrayList::class.java,
             SimpleDriverDataSource::class.java,
-            ChangeLogHistoryServiceFactory::class.java,
         ).forEach {
             hints.reflection().registerType(it, *MemberCategory.values())
         }
         hints.resources().registerPattern("db/changelog/db.changelog-master.sql")
+
+        // Might be fixed in
+        // https://github.com/oracle/graalvm-reachability-metadata/issues/431
+        listOf(
+            LoggerUIService::class.java,
+            ChangeLogHistoryServiceFactory::class.java,
+        ).forEach {
+            hints.reflection().registerType(it, *MemberCategory.values())
+        }
     }
 }
