a0ed8b40ad5cf5a8a900a2b2ef0b0e5d57a50170
hulkdx <hulktroll@gmail.com>
2025-11-17 16:27:36 +0200

    Update SQL

--- user-service/src/main/resources/db/changelog/db.changelog-master.sql
+++ user-service/src/main/resources/db/changelog/db.changelog-master.sql
@@ -60,25 +60,6 @@ CREATE TABLE professional_availability (
   )
 );
 
-CREATE TABLE booking_holds (
-  id                BIGSERIAL PRIMARY KEY,
-  user_id           BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-  idempotency_key   TEXT NOT NULL,
-  created_at        TIMESTAMPTZ NOT NULL,
-  expires_at        TIMESTAMPTZ NOT NULL,
-
-  CONSTRAINT booking_holds_user_ik_uk UNIQUE (user_id, idempotency_key)
-);
-CREATE INDEX IF NOT EXISTS idx_booking_holds_expires_at ON booking_holds (expires_at);
-
-CREATE TABLE booking_hold_items (
-  id                BIGSERIAL PRIMARY KEY,
-  hold_id           BIGINT NOT NULL REFERENCES booking_holds (id) ON DELETE CASCADE,
-  availability_id   BIGINT UNIQUE NOT NULL REFERENCES professional_availability (id) ON DELETE CASCADE,
-  created_at        TIMESTAMPTZ NOT NULL,
-  expires_at        TIMESTAMPTZ NOT NULL
-);
-
 CREATE TABLE bookings (
   id                       BIGSERIAL PRIMARY KEY,
   user_id                  BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
@@ -86,11 +67,18 @@ CREATE TABLE bookings (
   status                   TEXT NOT NULL,
   total_amount_cents       BIGINT NOT NULL,
   currency                 TEXT NOT NULL,
-  source_hold_id           BIGINT UNIQUE REFERENCES booking_holds(id) ON DELETE SET NULL,
   stripe_payment_intent_id TEXT UNIQUE,
   confirmed_at             TIMESTAMPTZ,
   canceled_at              TIMESTAMPTZ,
   failed_at                TIMESTAMPTZ,
   created_at               TIMESTAMPTZ NOT NULL,
   updated_at               TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE booking_items (
+  id                BIGSERIAL PRIMARY KEY,
+  booking_id        BIGINT NOT NULL REFERENCES bookings(id) ON DELETE CASCADE,
+  availability_id   BIGINT UNIQUE NOT NULL REFERENCES professional_availability (id) ON DELETE CASCADE,
+  created_at        TIMESTAMPTZ NOT NULL,
+  updated_at        TIMESTAMPTZ NOT NULL
 );
